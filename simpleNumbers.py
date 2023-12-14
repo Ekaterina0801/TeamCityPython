0de63e11d55e888687ce069a668ad3c391d7f452
Ekaterina0801 <ds.katrin@mail.ru>
2023-12-14 10:55:14 +0300

    add project

---
+++ simpleNumbers.py
@@ -0,0 +1,14 @@
+import math
+import random
+
+def is_prime(num, test_count):
+    for i in range(test_count):
+
+        rnd = random.randint(1, num - 1)
+
+        if (rnd ** (num - 1) % num != 1):
+            return False
+
+    return True
+
+print(is_prime(13, 10))
